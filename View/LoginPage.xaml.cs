959e79c4ab0efcea22e8352715c51bb8b07addec
thetharz <tharinduimalka915@gmail.com>
2023-07-14 09:10:20 +0530

    Add project files.

---
+++ View/LoginPage.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System.Windows;
+using System.Windows.Input;
+
+namespace Premium_Login_Window.View
+{
+    /// <summary>
+    /// Interaction logic for LoginPage.xaml
+    /// </summary>
+    public partial class LoginPage : Window
+    {
+        public LoginPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+            {
+                DragMove();
+            }
+        }
+
+        private void btnMinimize_Click(object sender, RoutedEventArgs e)
+        {
+            WindowState = WindowState.Minimized;
+        }
+
+        private void btnClose_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void btnMaxize_Click(object sender, RoutedEventArgs e)
+        {
+            WindowState= WindowState.Maximized;
+        }
+
+        private void txtUser_TextChanged(object sender, System.Windows.Controls.TextChangedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void btnLogin_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+    }
+}
